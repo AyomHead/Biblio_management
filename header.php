560e01f875347f83a4138d83cb483aa2913029d0
Ayomide Head <ezechielhan2005@gmail.com>
2025-09-15 09:49:44 +0100

    Barre de navigation

---
+++ header.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <i class="fas fa-book-open me-2"></i>BNB
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="index.php">
+                        <i class="fas fa-home me-1"></i>Accueil
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="catalogue.php">
+                        <i class="fas fa-book me-1"></i>Catalogue
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fas fa-info-circle me-1"></i>À propos
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">
+                        <i class="fas fa-envelope me-1"></i>Contactez-nous
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">
+                        <i class="fas fa-sign-in-alt me-1"></i>Connexion
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">
+                        <i class="fas fa-user-plus me-1"></i>Inscription
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
