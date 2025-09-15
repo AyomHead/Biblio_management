aee6dacd2370f4bb1463d4787836e4eed0d4b6fd
Ayomide Head <ezechielhan2005@gmail.com>
2025-09-15 09:46:08 +0100

    Pied de page

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="footer-logo">
+                    <i class="fas fa-book-open me-2"></i>BNB
+                </div>
+                <p class="footer-description">
+                    La Bibliothèque Nationale du Bénin préserve et promeut le patrimoine culturel et littéraire du pays.
+                </p>
+                <div class="social-icons">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+                <h5 class="footer-heading">Liens Rapides</h5>
+                <ul class="footer-links">
+                    <li><a href="#"><i class="fas fa-angle-right me-2"></i>Accueil</a></li>
+                    <li><a href="catalogue.php"><i class="fas fa-angle-right me-2"></i>Catalogue</a></li>
+                    <li><a href="#"><i class="fas fa-angle-right me-2"></i>À propos</a></li>
+                    <li><a href="#"><i class="fas fa-angle-right me-2"></i>Contactez-nous</a></li>
+                    <li><a href="#"><i class="fas fa-angle-right me-2"></i>Politique de confidentialité</a></li>
+                </ul>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+                <h5 class="footer-heading">Coordonnées</h5>
+                <ul class="footer-links">
+                    <li><i class="fas fa-map-marker-alt me-2"></i> Avenue de la Bibliothèque, Cotonou</li>
+                    <li><i class="fas fa-phone me-2"></i> +229 XX XX XX XX</li>
+                    <li><i class="fas fa-envelope me-2"></i> contact@bnb.bj</li>
+                    <li><i class="fas fa-clock me-2"></i> Lun-Ven: 8h-18h | Sam: 9h-13h</li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="copyright">
+            <p>&copy; 2023 Bibliothèque Nationale du Bénin. Tous droits réservés.</p>
+        </div>
+    </div>
+</footer>
